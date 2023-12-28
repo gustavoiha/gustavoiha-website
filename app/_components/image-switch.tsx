7c6103fdb134cbe63b462c76e2933e5fd0278e34
Gustavo Iha <gustavoiha@gmail.com>
2023-12-28 15:53:39 -0300

    feat: add scroll to image siwtch

--- app/_components/image-switch.tsx
+++ app/_components/image-switch.tsx
@@ -1,48 +1,27 @@
 'use client'
 
-import { useState } from 'react'
-
 import Image from 'next/image'
 
-interface Image {
+import './image-switch.css'
+
+type Image = {
   image: any;
   alternativeText: string;
   id: string;
 }
 
 export default function ImageSwitch ({ images, name }: { images: Array<Image>, name: string }) {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0)
-
-  const currentImage = images[currentImageIndex]
-
-  const handleSetCurrentImage = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setCurrentImageIndex(parseInt(event.target.value))
-  }
-
   return (
-    <div className="grid justify-items-center">
-      <Image
-        src={currentImage.image}
-        alt={currentImage.alternativeText}
-        className="rounded-3xl"
-      />
-
-      <fieldset className="flex flex-nowrap gap-3 mt-6">
-        {
-          images.map((image, index) => (
-            <input
-              className="appearance-none rounded-xl w-3 h-3 bg-slate-400 checked:bg-slate-600 cursor-pointer"
-              type="radio"
-              id={image.id}
-              key={image.id}
-              name={name}
-              value={index}
-              checked={index === currentImageIndex}
-              onChange={handleSetCurrentImage}
-            />
-          ))
-        }
-      </fieldset>
+    <div className="overflow-hidden rounded-3xl">
+      <div className="flex overflow-x-scroll image-switch">
+        {images.map((image) => (
+          <Image
+            src={image.image}
+            alt={image.alternativeText}
+            key={image.id}
+          />
+        ))}
+      </div>
     </div>
   )
 }
